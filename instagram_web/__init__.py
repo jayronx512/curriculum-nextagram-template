505a727d4bb124d645c911b3b83ffc424be4a49f
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-01-21 11:16:33 +0800

    data validation on emails, password validation with uppercase, lowercase, special characters and length

--- instagram_web/__init__.py
+++ instagram_web/__init__.py
@@ -4,6 +4,7 @@ from instagram_web.blueprints.users.views import users_blueprint
 from flask_assets import Environment, Bundle
 from .util.assets import bundles
 from models import user
+import re
 
 assets = Environment(app)
 assets.register(bundles)
@@ -27,11 +28,34 @@ def signup():
 def signup_form():
     username = request.form.get('username')
     email = request.form.get('email')
-    password = request.form.get('password')
-    new_user = user.User(username=username, email=email, password=password)
+    while True:
+        password = request.form.get('password')
+        if len(password) < 6:
+            flash('Make sure your password is at least 6 letters')
+            password_error = True
+            return render_template('signup.html', password_error = password_error)
+        elif re.search('[0-9]',password) is None:
+            flash('Make sure your password has a number in it')
+            password_error = True
+            return render_template('signup.html', password_error = password_error)
+        elif re.search('[A-Z]', password) is None:
+            flash('Make sure your password has a capital letter in it')
+            password_error = True
+            return render_template('signup.html', password_error = password_error)
+        elif re.search("[$&+,:;=?@#\"\\/|'<>.^*()%!-]", password) is None:
+            flash('Make sure your password has special character in it')
+            password_error = True
+            return render_template('signup.html', password_error = password_error)
+        else:
+            new_user = user.User(username=username, email=email, password=password)
+            break
     
     if new_user.save():
+        flash('New user created!')
         return redirect('signup')
 
     else:
-        return render_template('signup.html')
+        for err in new_user.errors:
+            flash(err)
+        # flash(user.User.errors[0])
+        return render_template('signup.html', errors=new_user.errors)
--- models/user.py
+++ models/user.py
@@ -6,3 +6,9 @@ class User(BaseModel):
     username = pw.CharField(unique=False)
     email = pw.CharField(unique=True)
     password = pw.CharField(unique=False)
+
+    def validate(self):
+        duplicate_emails = User.get_or_none(User.email == self.email)
+        if duplicate_emails:
+            self.errors.append('Email has been used')
+            
